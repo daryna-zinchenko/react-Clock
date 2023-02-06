b6109cea0039978edec66991e365e93139141a60
daryna-zinchenko <zinchenko.daryna@gmail.com>
2023-02-06 18:58:06 +0100

    Clock

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import './App.scss';
+import { Clock } from './components/Clock';
+
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
+}
+
+type State = {
+  clockName: string,
+  hasClock: boolean,
+};
+
+export class App extends Component<{}, State> {
+  state: Readonly<State> = {
+    clockName: 'Clock-0',
+    hasClock: true,
+  };
+
+  timerId = 0;
+
+  componentDidMount() {
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+
+    document.addEventListener('contextmenu', (event: MouseEvent) => {
+      event.preventDefault();
+      this.setState({ hasClock: false });
+    });
+
+    document.addEventListener('click', () => {
+      this.setState({ hasClock: true });
+    });
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+  }
+
+  render() {
+    const {
+      hasClock,
+      clockName,
+    } = this.state;
+
+    return (
+      <div className="App">
+        {hasClock && <Clock name={clockName} />}
+      </div>
+    );
+  }
+}
